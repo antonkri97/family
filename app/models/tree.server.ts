16fe4db18f6ea4e1194fa5c86501eae969922e18
krivokhizhin.a <krivokhizhin.a@selectel.ru>
2023-09-14 13:50:06 +0300

    delete unused code

--- app/models/tree.server.ts
+++ app/models/tree.server.ts
@@ -1,68 +1,20 @@
-import type { Tree } from "@prisma/client";
-import { prisma } from "~/db.server";
-import { GetPersonsListItems, getPersonListItems } from "./person.server";
-
-export async function createTree(data: Pick<Tree, "name" | "userId">) {
-  return (await prisma.tree.create({ data })).id;
-}
+import { getPersonListItems } from "./person.server";
+import type { PersonId } from "~/validators/person";
+import { personSchema } from "~/validators/person";
+import { buildTrees } from "./trees";
 
 export async function getTrees(userId: string) {
-  const persons = await getPersonListItems({ id: userId });
+  const personsRaw = await getPersonListItems({ id: userId });
+  const validated = personsRaw.map((person) => ({
+    ...personSchema.parse(person),
+    id: person.id as PersonId,
+  }));
 
-  const entitiesById: Map<string, (typeof persons)[number]> = new Map();
+  const entities = Object.fromEntries(
+    validated.map((person) => [person.id, person])
+  );
 
-  persons.forEach((person) => {
-    entitiesById.set(person.id, person);
-  });
+  const trees = buildTrees(validated, entities);
 
-  const fathers = toRoot(persons);
-
-  return fathers;
-}
-
-export function getTree({ id }: Pick<Tree, "id">) {
-  return prisma.tree.findFirst({
-    where: { id },
-  });
-}
-
-function toRoot(
-  persons: GetPersonsListItems
-): Record<string, { wifes: string[]; children: string[] }> {
-  const fathers: Record<
-    string,
-    { root: boolean; wifes: string[]; children: string[] }
-  > = {};
-
-  const entitiesById: Map<string, (typeof persons)[number]> = new Map();
-
-  persons.forEach((person) => {
-    entitiesById.set(person.id, person);
-  });
-
-  persons.forEach((person) => toRoot2(person));
-
-  function toRoot2(p: GetPersonsListItems[number]) {
-    const father = entitiesById.get(p?.fatherId ?? "");
-    if (p.fatherId && father) {
-      const { wifes, children } = fathers[p.fatherId] ?? {
-        wifes: [],
-        children: [],
-      };
-
-      p.motherId && wifes.push(p.motherId);
-      children.push(p.id);
-
-      fathers[p.fatherId] = { wifes, children, root: false };
-
-      toRoot2(father);
-    }
-
-    if (!p.fatherId) {
-    }
-
-    return;
-  }
-
-  return fathers;
+  return trees;
 }
